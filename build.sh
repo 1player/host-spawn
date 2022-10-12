1b0b10d7c4f0e15ff494e4d90f66a722ccab8fec
Stéphane Travostino <steph@combo.cc>
2022-10-12 12:02:39 +0100

    build: Create tarball with vendored sources. Fixes #14

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -euo pipefail
+
 ARCH="
     386-i386
     386-i686
@@ -8,12 +10,22 @@ ARCH="
     arm64-aarch64
 "
 
+ROOT_DIR=$(dirname "$0")
+VERSION=${VERSION:-HEAD}
+
+cd "$ROOT_DIR"
+
 mkdir -p build
 
-echo $VERSION
+echo "$VERSION"
 
 for architecture in ${ARCH}; do
 	CGO_ENABLED=0 GOARCH="$(echo "${architecture}" | cut -d'-' -f1)" go build \
 		-ldflags  "-X main.Version=$VERSION" \
 		-o build/host-spawn-"$(echo "${architecture}" | cut -d'-' -f2)"
 done
+
+# Create source tarball including vendored dependencies
+git clean -fdx -e build
+go mod vendor
+tar --create --zst --exclude build --file build/host-spawn-vendor.tar.zst "$ROOT_DIR"
